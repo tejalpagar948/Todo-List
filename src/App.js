e5deffe5a36307805987dc8e641ff39bf8c59604
ptejalaxioned <tejalp@axioned.com>
2025-07-26 19:45:57 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './output.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
+export default App;
